cca0403f1f3793ef370b29af1e47a585cd80c001
satwik.k <satwik.kondapalli@assettl.com>
2025-08-06 12:48:07 +0530

    2nd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,6 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { ReversePipe } from './reverse.pipe';
-import { ChildComponent } from "./child/child.component";
-import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
@@ -16,15 +13,5 @@ import { UserService } from './user.service';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
- isDarkMode = false;
 
- toggleDarkMode() {
-  this.isDarkMode = !this.isDarkMode;
-  const body = document.body;
-  if(this.isDarkMode) {
-    body.classList.add('dark-mode')
-  } else {
-    body.classList.remove('dark-mode')
-  }
- }
 }
